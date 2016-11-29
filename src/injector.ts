f4420169ea6fb9c241baf3fd907844e3a04b9a88
damian <carftjr@gmail.com>
2016-11-29 17:51:20 +0100

    add some strongtyping

--- src/injector.ts
+++ src/injector.ts
@@ -1,3 +1,8 @@
+export interface Provider {
+  class: string;
+  args: any[];
+}
+
 export class Injector {
   private resolvedProviders = new Map();
   private providers = new Map();
@@ -6,18 +11,18 @@ export class Injector {
     providers.forEach(provider => this.providers.set(provider.name, provider));
   }
 
-  get(...providers) {
+  get(...providers: Provider[]) {
     const resolvedProviders = providers.map(providerName => this.resolve(providerName));
     return resolvedProviders.length < 2 ? resolvedProviders[0] : resolvedProviders;
   }
 
-  resolveProvider(provider, args) {
+  resolveProvider(provider, args: any[]) {
     const instance = new provider(...args || [], this);
     this.resolvedProviders.set(provider.name, instance);
     return instance;
   }
 
-  resolve(provider) {
+  resolve(provider: Provider) {
     const { providers, resolvedProviders } = this;
     const providerName = typeof provider === 'object' ? provider.class : provider;
     
