b3ac559074a176127febb5152fb4f0f5a6131292
Damian <rascada@users.noreply.github.com>
2016-11-29 02:38:52 +0100

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,38 @@
+class Injector {
+  private resolvedProviders = new Map();
+  private providers = new Map();
+
+  constructor(private providers = []) {
+    providers.forEach(provider => this.providers.set(provider.name, provider));
+  }
+
+  resolveProvider(provider, args) {
+    const instance = new provider(...args || [], this);
+    this.resolvedProviders.set(provider.name, instance);
+    return instance;
+  }
+
+  get(...providers) {
+    const resolvedProviders = providers
+      .map(providerName => this.resolve(providerName));
+    
+    return resolvedProviders.length < 2 
+      ? resolvedProviders[0]
+      : resolvedProviders;
+  }
+
+  resolve(provider) {
+    const { providers, resolvedProviders } = this;
+    const providerName = typeof provider === 'object' 
+      ? provider.class
+      : provider;
+    
+    if (resolvedProviders.has(providerName)) {
+      return resolvedProviders.get(providerName);
+    } else if (providers.has(providerName)) {
+      return this.resolveProvider(providers.get(providerName), provider.args);      
+    } else {
+      throw new Error(`Missing class ${providerName}`);
+    }
+  }
+}
